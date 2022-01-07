c38fce631f6db455c907824d5bf08cca84b48f09
Sanya0909 <skuckarov8@gmail.com>
2022-01-07 15:27:19 +0500

    scan change

--- lib/scan_camera.dart
+++ lib/scan_camera.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text('AAA'),
+        ),
+      ],
+    );
+  }
+}
